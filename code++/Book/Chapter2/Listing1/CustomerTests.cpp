681003a18026e064833300683f89a51385f4c200
Yury Kuznetsov <yyk99@kestrel.home>
2025-02-22 09:38:48 -0500

    Update (WIP)

--- code++/Book/Chapter2/Listing1/CustomerTests.cpp
+++ code++/Book/Chapter2/Listing1/CustomerTests.cpp
@@ -1,39 +1,45 @@
-﻿using Xunit;
+﻿#include "gtest/gtest.h"
 
-namespace Book.Chapter2.Listing1
+#include "Other.h"
+
+namespace Book::Chapter2::Listing1
 {
-    public class CustomerTests
-    {
-        [Fact]
-        public void Purchase_succeeds_when_enough_inventory()
-        {
-            // Arrange
-            var store = new Store();
-            store.AddInventory(Product.Shampoo, 10);
-            var customer = new Customer();
-
-            // Act
-            bool success = customer.Purchase(store, Product.Shampoo, 5);
-
-            // Assert
-            Assert.True(success);
-            Assert.Equal(5, store.GetInventory(Product.Shampoo));
-        }
-
-        [Fact]
-        public void Purchase_fails_when_not_enough_inventory()
-        {
-            // Arrange
-            var store = new Store();
-            store.AddInventory(Product.Shampoo, 10);
-            var customer = new Customer();
-
-            // Act
-            bool success = customer.Purchase(store, Product.Shampoo, 15);
-
-            // Assert
-            Assert.False(success);
-            Assert.Equal(10, store.GetInventory(Product.Shampoo));
-        }
-    }
+#if 0
 }
+#endif
+  
+class CustomerTests : public testing::Test
+{
+
+};
+
+TEST_F(CustomerTests, Purchase_succeeds_when_enough_inventory)
+{
+    // Arrange
+    var store = new Store();
+    store.AddInventory(Product.Shampoo, 10);
+    var customer = new Customer();
+
+    // Act
+    bool success = customer.Purchase(store, Product.Shampoo, 5);
+
+    // Assert
+    Assert.True(success);
+    Assert.Equal(5, store.GetInventory(Product.Shampoo));
+}
+
+TEST_F(CustomerTests, Purchase_fails_when_not_enough_inventory)
+{
+    // Arrange
+    var store = new Store();
+    store.AddInventory(Product.Shampoo, 10);
+    var customer = new Customer();
+
+    // Act
+    bool success = customer.Purchase(store, Product.Shampoo, 15);
+
+    // Assert
+    Assert.False(success);
+    Assert.Equal(10, store.GetInventory(Product.Shampoo));
+}
+} // namespace
---
+++ code++/Book/Chapter2/Listing1/Other.h
@@ -0,0 +1,80 @@
+﻿#ifndef __OTHER_H
+#define __OTHER_H
+
+#include <map>
+#include <exception>
+
+namespace book::chapter2::listing1 {
+#if 0
+}
+#endif
+
+enum Product
+{
+    Shampoo,
+    Book
+};
+
+class Store
+{
+private:
+    std::map<Product, int> _inventory;
+
+public:
+    bool HasEnoughInventory(Product product, int quantity)
+    {
+        return GetInventory(product) >= quantity;
+    }
+
+    void RemoveInventory(Product product, int quantity)
+    {
+        if (!HasEnoughInventory(product, quantity))
+        {
+            throw std::runtime_error("Not enough inventory");
+        }
+
+        _inventory[product] -= quantity;
+    }
+
+    void AddInventory(Product product, int quantity)
+    {
+        if (_inventory.count(product))
+        {
+            _inventory[product] += quantity;
+        }
+        else
+        {
+            _inventory[product] = quantity;
+        }
+    }
+
+    int GetInventory(Product product)
+    {
+#if 0
+        bool productExists = _inventory.TryGetValue(product, out int remaining);
+        return productExists ? remaining : 0;
+#else
+        return _inventory.count(product);
+#endif
+    }
+};
+
+class Customer
+{
+public:
+    bool Purchase(Store store, Product product, int quantity)
+    {
+        if (!store.HasEnoughInventory(product, quantity))
+        {
+            return false;
+        }
+
+        store.RemoveInventory(product, quantity);
+
+        return true;
+    }
+};
+
+
+} // namespace
+#endif
