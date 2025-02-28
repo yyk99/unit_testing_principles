9556b9276fdf40911ffd8b9776fa7bb9b4cbb312
Yura K <yyk@silvana.home>
2025-02-28 05:35:07 -0500

    WIP

---
+++ code++/Book/Chapter3/CustomerTests_3/CustomerTests.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+// Book/Chapter3/CustomerTests_3/CustomerTests.cpp
+// Book/Chapter2/Listing1/Other.h
+
+#include "../../Chapter2/Listing1/Other.h"
+
+using namespace book::chapter2::listing1;
+// using Xunit;
+
+namespace Book::Chapter3::CustomerTests_3
+{
+#if 0
+}
+#endif
+
+class CustomerTests: public testing::Test
+{
+protected:
+    Store _store;
+    Customer _sut;
+
+public:
+    CustomerTests()
+    {
+        _store.AddInventory(Product::Shampoo,10);
+    }
+};
+
+// [Fact]
+TEST_F(CustomerTests,Purchase_succeeds_when_enough_inventory)
+{
+    bool success = _sut.Purchase(*_store,Product::Shampoo,5);
+
+    ASSERT_TRUE(success);
+    ASSERT_EQ(5,_store.GetInventory(Product::Shampoo));
+}
+
+//    [Fact]
+TEST_F(CuPurchase_fails_when_not_enough_inventory()
+       {
+           bool success = _sut.Purchase(_store,Product.Shampoo,15);
+
+           Assert.False(success);
+           Assert.Equal(10,_store.GetInventory(Product.Shampoo));
+       }
+}
+}
