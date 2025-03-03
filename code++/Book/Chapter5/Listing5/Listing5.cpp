dbff310460fea3cab565a7a711f18cc1f9ccdc3b
Yury Kuznetsov <yyk99@kestrel.home>
2025-03-03 05:41:16 -0500

    Update Book/Chapter5

---
+++ code++/Book/Chapter5/Listing5/Listing5.cpp
@@ -0,0 +1,43 @@
+﻿#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <string>
+
+class User
+{
+public:
+    std::string Name;
+
+    std::string NormalizeName(std::string name)
+    {
+        std::string result = name; // TODO: (name ?? "").Trim();
+
+        if (result.size() > 50)
+            return result.substr(0, 50);
+
+        return result;
+    }
+};
+
+class UserController
+{
+public:
+    void RenameUser(int userId, std::string newName)
+    {
+        User user = GetUserFromDatabase(userId);
+
+        std::string normalizedName = user.NormalizeName(newName);
+        user.Name = normalizedName;
+
+        SaveUserToDatabase(user);
+    }
+private:
+    void SaveUserToDatabase(User user)
+    {
+    }
+
+    User GetUserFromDatabase(int userId)
+    {
+        return User();
+    }
+};
